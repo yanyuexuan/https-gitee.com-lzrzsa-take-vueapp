74445c46bceff52b1b8cb233fc8d5df87fe8a044
chendan <1432640335@qq.com>
2021-02-22 16:08:38 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-    
-    <!-- <CountIndex />
-    <hr />
-    <TodoListIndex />
-    <hr />
-    <SlotsIndex >
-      <template v-slot:test="{count,add,minus}">
-        <CountShow :count="count"/>
-        <CountIButton name="+" @click="add" />
-        <CountIButton name="-" @click="minus" />
-      </template>
-    </SlotsIndex>
-    <StudentsIndex /> -->
     <router-view/>
   </div>
 </template>
