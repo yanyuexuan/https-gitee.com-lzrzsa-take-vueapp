80cb4c0a1dbb8957318c13a6e40a1fedf96ccc1f
yanyuexuan <569172937@qq.com>
2021-02-22 16:12:52 +0800

    修改key

--- src/views/info.vue
+++ src/views/info.vue
@@ -1,32 +1,36 @@
 <template>
- <a-layout id="components-layout-demo-side" style="min-height: 100vh" >
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
     <a-layout-sider v-model="collapsed" collapsible>
       <div class="logo" />
-      <a-menu theme="dark" :default-selected-keys="['1']" mode="inline"  @click="handleClick">
+      <a-menu
+        theme="dark"
+        :default-selected-keys="['1']"
+        mode="inline"
+        @click="handleClick"
+      >
         <a-menu-item key="1">
           <a-icon type="pie-chart" />
           <span>用户管理</span>
         </a-menu-item>
         <a-sub-menu key="sub1">
           <span slot="title"><a-icon type="shop" /><span>admin</span></span>
-           <a-menu-item key="/info/shop/addshop">
+          <a-menu-item key="/info/shop/addshop">
             新增
           </a-menu-item>
           <a-menu-item key="/info/shop">
             所有
           </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub1">
+        <a-sub-menu key="sub2">
           <span slot="title"><a-icon type="shop" /><span>店铺</span></span>
-           <a-menu-item key="/info/shop/addshop">
+          <a-menu-item key="/info/shop/addshop">
             新增店铺
           </a-menu-item>
           <a-menu-item key="/info/shop">
             所有店铺
           </a-menu-item>
-         
         </a-sub-menu>
-        <a-sub-menu key="sub2">
+        <a-sub-menu key="sub3">
           <span slot="title"><a-icon type="team" /><span>商品</span></span>
           <a-menu-item key="6">
             Team 1
@@ -35,7 +39,7 @@
             Team 2
           </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
+        <a-sub-menu key="sub4">
           <span slot="title"><a-icon type="team" /><span>订单</span></span>
           <a-menu-item key="6">
             Team 1
@@ -44,7 +48,7 @@
             Team 2
           </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
+        <a-sub-menu key="sub5">
           <span slot="title"><a-icon type="team" /><span>优惠劵</span></span>
           <a-menu-item key="6">
             Team 1
@@ -56,15 +60,17 @@
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0" >
+      <a-layout-header style="background: #fff; padding: 0">
         1234
-      </a-layout-header >
+      </a-layout-header>
       <a-layout-content style="margin: 0 16px">
         <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item>User</a-breadcrumb-item>
           <a-breadcrumb-item>Bill</a-breadcrumb-item>
         </a-breadcrumb>
-        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+        <div
+          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
+        >
           <router-view />
         </div>
       </a-layout-content>
@@ -92,23 +98,17 @@
 </template>
 
 <script>
-
-
 export default {
-  
   data() {
     return {
-       collapsed: false,
+      collapsed: false
     };
   },
   methods: {
-    handleClick({  key }) {
-      this.$router.push(
-            {path: `${key}`},
-            
-         )
-    },
-  },
+    handleClick({ key }) {
+      this.$router.push({ path: `${key}` });
+    }
+  }
 };
 </script>
 
