c88c56fdbf655aa2d83972f7493a2fea0138777c
zhangjiajun <1176713018@qq.com>
2021-02-23 19:01:45 +0800

    改冲突

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,6 @@ import AddAdmin from "../components/admin/addAdmin.vue";
 import commodityRouter from "./commodity";
 import couponsRouter from "./coupons";
 import ordersRouter from "./orders";
-import Login from "../views/admin.vue";
-import admin from "./admin";
 import shopsRouter from "./shops";
 
 Vue.use(VueRouter);
